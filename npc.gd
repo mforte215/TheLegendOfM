c8f6a347ab87de0eb060ac1f53b9d69479ddd531
mforte215 <mark.fortee@gmail.com>
2026-03-06 10:39:22 -0500

    Phase 4 in progress - dialogue system with typewriter effect and NPC interaction

---
+++ npc.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var speaker_name: String = ""
+@export var dialogue_lines: Array = []
+
+var player_nearby: bool = false
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		player_nearby = true
+
+func _on_body_exited(body: Node) -> void:
+	if body.is_in_group("player"):
+		player_nearby = false
+
+func _input(event: InputEvent) -> void:
+	if not player_nearby:
+		return
+	if DialogueBox.is_open:
+		return
+	if Input.is_action_just_pressed("interact"):
+		DialogueBox.start(dialogue_lines, speaker_name)
