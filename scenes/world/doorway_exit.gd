1641e8ad3686ecd6c046d78a7d7a3ca035b77572
mforte215 <mark.fortee@gmail.com>
2026-03-06 13:28:19 -0500

    Save/load system complete - stats and inventory persisted to disk

--- scenes/world/doorway_exit.gd
+++ scenes/world/doorway_exit.gd
@@ -4,6 +4,8 @@ extends Area2D
 @export var is_enabled: bool = true
 
 func _ready() -> void:
+	collision_layer = 8  # triggers
+	collision_mask = 2   # player
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node) -> void:
